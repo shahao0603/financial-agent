dccfe66120710748aff60a95ca41a7605016312e
shahao0603 <a0225777255@gmail.com>
2026-09-18 13:27:23 +0800

    Update Discord webhook URL in bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 from datetime import datetime
 
-WEBHOOK_URL = "https://discord.com/api/webhooks/1550338399699734628/wnm5et6lVHiMjpFD2gdKs3XefCXxOH-vlCz5UeMHotZc7MWzeh_e4CQb2NIwxugBUKn4"
+WEBHOOK_URL = "https://discordapp.com/api/webhooks/1550377530228477974/qctcT78BZLT56aQW7skxtULN00drer8be6eEdhJT-ocwYp5mpiLOfq8Ef6pSPKFvMl28"
 
 def get_market_summary():
     today = datetime.now().strftime("%Y-%m-%d")
